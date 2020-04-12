c13a5cc46f7e28df8bf6da9e91199a6930768ce6
shunichiiiii <shun141.0220.bbc@i.softbank.jp>
2020-04-12 16:06:33 +0900

    new_post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_many :likes
   has_many :comments
-  has_many :post_images
+  has_one_attached :image
 end
